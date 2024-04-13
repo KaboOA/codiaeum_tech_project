bccf8c2d8e5c5a6a7f66f8386ab92b7d4b5bef32
KaboOA <ahmedkabary80@gmail.com>
2024-04-13 22:09:05 +0200

    4

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:codiaeum_tech_project/Transactions.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: Locale('ar'),
+      themeMode: ThemeMode.light,
+      home: TransactionsScreen(),
+    );
   }
 }
