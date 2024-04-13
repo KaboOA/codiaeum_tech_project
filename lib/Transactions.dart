6320bb29ca4c1c3623c385e0a0e9522c742af8de
lWAHBAl <>
2024-04-13 23:16:27 +0200

    layout

--- lib/Transactions.dart
+++ lib/Transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TransactionsScreen extends StatelessWidget {
   const TransactionsScreen({super.key});
@@ -8,37 +9,128 @@ class TransactionsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Padding(
-          padding: EdgeInsets.only(left: 20),
-          child: Text('Home'),
+          padding: EdgeInsetsDirectional.all(25),
+          child: Text(
+            'Home',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
         ),
       ),
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 25),
-            child: Text(
+      body: Padding(
+        padding: const EdgeInsetsDirectional.only(start: 40),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
               'EGP 3,250',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            'Current balance ',
-            style: TextStyle(fontSize: 20, color: Colors.grey),
-          ),
-          Row(
-            children: [
-              IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
-              const SizedBox(
-                width: 30,
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              'Current balance ',
+              style: TextStyle(fontSize: 20, color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(start: 40.0),
+              child: Row(
+                children: [
+                  InkWell(
+                    borderRadius: BorderRadius.circular(30),
+                    onTap: () {},
+                    child: CircleAvatar(
+                      minRadius: 30,
+                      backgroundColor: Colors.blue[70],
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  InkWell(
+                    borderRadius: BorderRadius.circular(30),
+                    onTap: () {},
+                    child: CircleAvatar(
+                      minRadius: 30,
+                      backgroundColor: Colors.blue[70],
+                      child: const Icon(
+                        Icons.send,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  )
+                ],
               ),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.send))
-            ],
-          )
-        ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Expanded(
+                child: ListView.separated(
+              itemBuilder: (context, index) => builditem(),
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 20,
+              ),
+              itemCount: 5,
+            ))
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget builditem() {
+    return const Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(bottom: 10),
+          child: CircleAvatar(
+            radius: 25,
+            child: Icon(
+              Icons.arrow_upward,
+              size: 25,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        SizedBox(
+          width: 500,
+          // color: Colors.red,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Transaction is Wrtitten here ',
+                maxLines: 3,
+                style: TextStyle(fontSize: 25),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'date here',
+                style: TextStyle(color: Colors.grey),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+      ],
     );
   }
 }
