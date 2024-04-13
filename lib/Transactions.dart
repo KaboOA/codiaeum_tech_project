a3b7b5682253dc594e178eafeac8d56a7d60d2ae
lWAHBAl <>
2024-04-13 22:38:25 +0200

    7

--- lib/Transactions.dart
+++ lib/Transactions.dart
@@ -12,10 +12,33 @@ class TransactionsScreen extends StatelessWidget {
           child: Text('Home'),
         ),
       ),
-      body: Center(
-          child: Container(
-        child: const Text('Transactions'),
-      )),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 25),
+            child: Text(
+              'EGP 3,250',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'Current balance ',
+            style: TextStyle(fontSize: 20, color: Colors.grey),
+          ),
+          Row(
+            children: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+              const SizedBox(
+                width: 30,
+              ),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.send))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
