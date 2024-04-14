ecb6f661721741273d8f26efc17200b65e708bdf
KaboOA <ahmedkabary80@gmail.com>
2024-04-14 14:57:05 +0200

    edit cubit and states

--- lib/TransactionsCubit.dart
+++ lib/TransactionsCubit.dart
@@ -1,7 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:codiaeum_tech_project/DataModel.dart';
 import 'package:codiaeum_tech_project/TransactionsStates.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/TransactionsStates.dart
+++ lib/TransactionsStates.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
-
 abstract class TransactionsStates {}
 
 class InitialState extends TransactionsStates {}
